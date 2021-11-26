de22d85ef701588ea52f46b5c76cf1721835f1f4
VanDeGraf <vandegraf@bk.ru>
2021-11-26 14:39:36 +0400

    Install webpack css and image loaders

--- webpack.config.js
+++ webpack.config.js
@@ -12,4 +12,16 @@ module.exports = {
             directory: path.join(__dirname, 'dist'),
         },
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
